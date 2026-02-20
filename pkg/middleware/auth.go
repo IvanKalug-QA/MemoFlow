fa4da57628d58c23cbb5e5102f5b31073fb49608
IVAN-GUY <kaluginivan2002@mail.ru>
2026-02-20 22:18:55 +0300

    add error unauthed in auth router

--- pkg/middleware/auth.go
+++ pkg/middleware/auth.go
@@ -14,11 +14,24 @@ const (
 	ContextEmailKey key = "ContextEmailKey"
 )
 
+func WriteUnauthed(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusUnauthorized)
+	w.Write([]byte(http.StatusText(http.StatusUnauthorized)))
+}
+
 func IsAuthed(next http.Handler, config *configs.Config) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		bearer := r.Header.Get("Authorization")
+		if !strings.HasPrefix(bearer, "Bearer ") {
+			WriteUnauthed(w)
+			return
+		}
 		token := strings.TrimPrefix(bearer, "Bearer ")
-		_, data := jwt.NewJWT(config.Auth.Secret).Parse(token)
+		is_valid, data := jwt.NewJWT(config.Auth.Secret).Parse(token)
+		if !is_valid {
+			WriteUnauthed(w)
+			return
+		}
 		ctx := context.WithValue(r.Context(), ContextEmailKey, data.Email)
 		req := r.WithContext(ctx)
 		next.ServeHTTP(w, req)
