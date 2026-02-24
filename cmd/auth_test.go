5189b38b1715711133acf27620271a434033528e
IVAN-GUY <kaluginivan2002@mail.ru>
2026-02-24 09:41:29 +0300

    add failed resp test and add check body

--- cmd/auth_test.go
+++ cmd/auth_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"io"
 	"memoflow/internal/auth"
 	"net/http"
 	"net/http/httptest"
@@ -24,5 +25,35 @@ func TestLoginSuccess(t *testing.T) {
 	}
 	if resp.StatusCode != http.StatusOK {
 		t.Fatalf("Expected %d got %d", http.StatusOK, resp.StatusCode)
+	}
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var resData auth.LoginResponse
+	err = json.Unmarshal(body, &resData)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resData.Token == "" {
+		t.Fatal("Token empty")
+	}
+}
+
+func TestLoginFailed(t *testing.T) {
+	ts := httptest.NewServer(App())
+	defer ts.Close()
+
+	data, _ := json.Marshal(&auth.LoginRequest{
+		Email:    "test@mail.ru",
+		Password: "20022002",
+	})
+
+	resp, err := http.Post(ts.URL+"/auth/login", "application/json", bytes.NewReader(data))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != http.StatusUnauthorized {
+		t.Fatalf("Expected %d got %d", http.StatusUnauthorized, resp.StatusCode)
 	}
 }
