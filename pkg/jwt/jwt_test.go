1e6d522870e48d87cdddf03e5ddeaaafb841c7e3
IVAN-GUY <kaluginivan2002@mail.ru>
2026-02-24 10:59:01 +0300

    add jwt unit test

---
+++ pkg/jwt/jwt_test.go
@@ -0,0 +1,24 @@
+package jwt_test
+
+import (
+	"memoflow/pkg/jwt"
+	"testing"
+)
+
+func TestJWTCreate(t *testing.T) {
+	const email = "a@a.ru"
+	jwtService := jwt.NewJWT("aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1kUXc0dzlXZ1hjUQ==")
+	token, err := jwtService.Create(jwt.JWTData{
+		Email: email,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	isValid, data := jwtService.Parse(token)
+	if !isValid {
+		t.Fatal("Token is invalid")
+	}
+	if data.Email != email {
+		t.Fatalf("Email %s not equal %s", data.Email, email)
+	}
+}
