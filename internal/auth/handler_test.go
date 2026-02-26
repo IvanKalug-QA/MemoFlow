f714c846b1f3056f358b869c63b232de03c6daae
IVAN-GUY <kaluginivan2002@mail.ru>
2026-02-26 20:14:27 +0300

    add test for login

--- internal/auth/handler_test.go
+++ internal/auth/handler_test.go
@@ -1,10 +1,14 @@
 package auth_test
 
 import (
+	"bytes"
+	"encoding/json"
 	"memoflow/configs"
 	"memoflow/internal/auth"
 	"memoflow/internal/user"
 	"memoflow/pkg/db"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
@@ -12,18 +16,16 @@ import (
 	"gorm.io/gorm"
 )
 
-func TestLoginSuccess(t *testing.T) {
+func bootsrap() (*auth.AuthHandler, sqlmock.Sqlmock, error) {
 	database, mock, err := sqlmock.New()
 	if err != nil {
-		t.Fatal("Failed init mock db")
-		return
+		return nil, nil, err
 	}
 	gormDb, err := gorm.Open(postgres.New(postgres.Config{
 		Conn: database,
 	}))
 	if err != nil {
-		t.Fatal("Failed init mock db")
-		return
+		return nil, nil, err
 	}
 	userRepo := user.NewUserRepository(&db.Db{DB: gormDb})
 	handler := auth.AuthHandler{
@@ -33,5 +35,29 @@ func TestLoginSuccess(t *testing.T) {
 			},
 		},
 		AuthService: auth.NewAuthService(userRepo),
+	}
+	return &handler, mock, nil
+}
+
+func TestLoginSuccess(t *testing.T) {
+	handler, mock, err := bootsrap()
+	rows := sqlmock.NewRows([]string{"email", "password"}).
+		AddRow("user@mail.ru", "$2a$10$yoVd4mPFxg62HrIO4zdvq.JZ0WYqameV56XNh3Qvrwz3oFqE1MMG2")
+	mock.ExpectQuery("SELECT").WillReturnRows(rows)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+
+	data, _ := json.Marshal(&auth.LoginRequest{
+		Email:    "user@mail.ru",
+		Password: "20022002",
+	})
+	reader := bytes.NewReader(data)
+	w := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodPost, "/auth/login", reader)
+	handler.Login()(w, req)
+	if w.Code != http.StatusOK {
+		t.Errorf("got %d, expected %d", w.Code, http.StatusOK)
 	}
 }
